97cbfd8c998523b4f4c908fa5f407b9a92050b9a
Alex Hewitt <hewittaj@outlook.com>
2022-03-30 16:31:43 -0500

    added button event listeners, other css/html changes

--- script.js
+++ script.js
@@ -1,9 +1,46 @@
-
+// Variables
+firstRun = true;
 
 // This function adds two numbers
 function add(num1, num2){
     return num1 + num2;
 };
+
+
+// This function adds a listener to the buttons
+function addListenerToButtons(){
+    // Variables
+    let buttons = document.querySelectorAll("button");
+    let arr = Array.from(buttons);
+    let display = document.querySelector("#display-text");
+
+    // Loop through the buttons and add a listener to each one
+    for(let button of arr){
+        let val = parseInt(button.value);
+
+        // If the button is not a number
+        if(isNaN(val)){
+            continue;
+        }
+
+        // If the button is a number
+        else{
+            button.addEventListener('click', () => {
+                // Detect if this is the first button being entered and change firstRun to false
+                if(firstRun){
+                    display.value = button.textContent;
+                    firstRun = false;
+                }
+
+                // If this is not the first button being entered append the number to the display
+                else{
+                    display.value = display.value + button.textContent;
+                }
+            });
+        }       
+    };
+}
+
 
 // This function divides two numbers
 function divide(num1, num2){
@@ -42,3 +79,10 @@ function operate(operator, num1, num2){
 function subtract(num1, num2){
     return num1 - num2;
 };
+
+// This function calls the main functions to setup the calculator
+function mainCall(){
+    addListenerToButtons();
+}
+
+mainCall();
